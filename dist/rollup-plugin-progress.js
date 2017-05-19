daa7771ce77d8967938c4a5eea35e4a1dc5ba047
Vladimir Alabov <alabov.v.n@gmail.com>
2017-05-19 16:32:27 +0300

    Added percent indicator

--- dist/rollup-plugin-progress.js
+++ dist/rollup-plugin-progress.js
@@ -2,14 +2,14 @@
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var fs = require('fs');
+var fs = _interopDefault(require('fs'));
 var path = _interopDefault(require('path'));
 var chalk = _interopDefault(require('chalk'));
 var readline = require('readline');
 var rollupPluginutils = require('rollup-pluginutils');
 
 function normalizePath(id) {
-	return path.relative(process.cwd(), id).split(path.sep).join('/');
+  return path.relative(process.cwd(), id).split(path.sep).join('/');
 }
 
 function progress(options) {
@@ -20,7 +20,14 @@ function progress(options) {
   }
 
   var filter = rollupPluginutils.createFilter(options.include, options.exclude);
+  var total = 0;
+  try {
+    total = fs.readFileSync(totalFilePath);
+  } catch (e) {
+    fs.writeFileSync(totalFilePath, 0);
+  }
   var progress = {
+    total: total,
     loaded: 0
   };
 
@@ -39,14 +46,19 @@ function progress(options) {
       if (options.clearLine && process.stdin.isTTY) {
         process.stdout.clearLine();
         process.stdout.cursorTo(0);
-        var output = "(" + (chalk.red(progress.loaded)) + "): " + file;
+    var output = "";
+        if (progress.total > 0) {
+          var percent = Math.round(100 * progress.loaded / progress.total);
+          output += Math.min(100, percent) + "% ";
+        }
+        output += "(" + (chalk.red(progress.loaded)) + "): " + file;
         if (output.length < process.stdout.columns) {
           process.stdout.write(output);
         } else {
           process.stdout.write(output.substring(0, process.stdout.columns - 1));
         }
       } else {
-        console.log(("(" + (chalk.red(progress.loaded)) + "): file"));
+        console.log(("(" + (chalk.red(progress.loaded)) + "): " + file));
       }
     },
     ongenerate: function ongenerate() {
