4d22fae78f3f8e42951d2bba092a2c313289236f
Uk-Jin Jang <doublestat@gmail.com>
2019-01-04 17:36:13 +0900

    Update for supporting Rollup v1.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,11 @@ import buble from 'rollup-plugin-buble';
 const external = Object.keys(require('./package.json').dependencies).concat(['fs', 'path', 'readline']);
 
 export default {
-  entry: 'src/index.js',
+  input: 'src/index.js',
   plugins: [ buble() ],
   external: external,
-  targets: [
-    { dest: 'dist/rollup-plugin-progress.js', format: 'cjs' },
-    { dest: 'dist/rollup-plugin-progress.esm.js', format: 'es' }
+  output: [
+    { file: 'dist/rollup-plugin-progress.js', format: 'cjs' },
+    { file: 'dist/rollup-plugin-progress.esm.js', format: 'es' }
   ]
 };
